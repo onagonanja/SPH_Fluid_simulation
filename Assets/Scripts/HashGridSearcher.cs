bd979841b8d30a880a4238ea6fd0e50b620ad581
takepoyo <takepoyo191101@gmail.com>
2025-04-26 15:24:33 +0900

    Implement simple SPH

---
+++ Assets/Scripts/HashGridSearcher.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HashGridSearcher {
+    Vector3 gridBaseMin;
+    Vector3 gridBaseMax;
+    float gridSize;
+    HashSet<int>[][][] buket;
+
+    public HashGridSearcher() {
+        GameObject sph = GameObject.Find("SPH_solver");
+        SPH_solver solver = sph.GetComponent<SPH_solver>();
+        gridBaseMin = new Vector3(-solver.BOX_SIZE, -solver.BOX_SIZE, -solver.BOX_SIZE) * 10;
+        gridBaseMax = new Vector3(solver.BOX_SIZE, solver.BOX_SIZE, solver.BOX_SIZE) * 10;
+        gridSize = solver.particleRadius * 2f;
+        int gridSizeX = Mathf.CeilToInt((gridBaseMax.x - gridBaseMin.x) / gridSize);
+        int gridSizeY = Mathf.CeilToInt((gridBaseMax.y - gridBaseMin.y) / gridSize);
+        int gridSizeZ = Mathf.CeilToInt((gridBaseMax.z - gridBaseMin.z) / gridSize);
+        buket = new HashSet<int>[gridSizeX][][];
+
+        for (int i = 0; i < gridSizeX; i++) {
+            buket[i] = new HashSet<int>[gridSizeY][];
+            for (int j = 0; j < gridSizeY; j++) {
+                buket[i][j] = new HashSet<int>[gridSizeZ];
+                for (int k = 0; k < gridSizeZ; k++) {
+                    buket[i][j][k] = new HashSet<int>();
+                }
+            }
+        }
+    }
+
+    public void UpdateHashGrid(Particle[] particles) {
+        Clear();
+        for (int i = 0; i < particles.Length; i++) {
+            Particle p = particles[i];
+            Vector3 pos = p.transform.position;
+            int x = Mathf.FloorToInt((pos.x - gridBaseMin.x) / gridSize);
+            int y = Mathf.FloorToInt((pos.y - gridBaseMin.y) / gridSize);
+            int z = Mathf.FloorToInt((pos.z - gridBaseMin.z) / gridSize);
+
+            if (x >= 0 && x < buket.Length && y >= 0 && y < buket[x].Length && z >= 0 && z < buket[x][y].Length) {
+                buket[x][y][z].Add(i);
+            }
+        }
+    }
+
+    public void forEachNeighbor(Particle[] particles, System.Action<int, int> action) {
+        for (int i = 0; i < particles.Length; i++) {
+            Particle p = particles[i];
+            Vector3 pos = p.transform.position;
+            int x = Mathf.FloorToInt((pos.x - gridBaseMin.x) / gridSize);
+            int y = Mathf.FloorToInt((pos.y - gridBaseMin.y) / gridSize);
+            int z = Mathf.FloorToInt((pos.z - gridBaseMin.z) / gridSize);
+
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    for (int dz = -1; dz <= 1; dz++) {
+                        int nx = x + dx;
+                        int ny = y + dy;
+                        int nz = z + dz;
+
+                        if (nx >= 0 && nx < buket.Length && ny >= 0 && ny < buket[nx].Length && nz >= 0 && nz < buket[nx][ny].Length) {
+                            foreach (int neighborIndex in buket[nx][ny][nz]) {
+                                action(i, neighborIndex);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void Clear() {
+        for (int i = 0; i < buket.Length; i++) {
+            for (int j = 0; j < buket[i].Length; j++) {
+                for (int k = 0; k < buket[i][j].Length; k++) {
+                    buket[i][j][k].Clear();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour {
+    public Vector3 v;
+    public Vector3 a;
+    public float density = 0f;
+    public float mass = 0.04f;
+    public float pressure = 0f;
+    public Vector3 pressureGradient = Vector3.zero;
+    public Vector3 viscosity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start() {
+        v = new Vector3(0, 0, 0);
+        a = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
